5aeae1437d259a09bb679ac153faaa7eef29400f
Bryan Muller <bmuller0975@gmail.com>
2019-01-12 01:11:25 -0700

    drop schema properly

--- jest/globalTeardown.js
+++ jest/globalTeardown.js
@@ -1,8 +1,9 @@
 require("ts-node/register");
-const Redis = require('ioredis')
+const Redis = require('ioredis');
+const {dropSchema} = require('./teardownTestServer');
 
 module.exports = async () => {
   const redis = new Redis(process.env.REDIS_URL);
-  await redis.flushall();
+  await Promise.all([redis.flushall(), dropSchema()]);
   return null;
 };
