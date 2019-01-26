48b78dd299472a3f94dd37379a988d07afc9c417
Bryan Muller <bmuller0975@gmail.com>
2019-01-25 20:13:36 -0700

    create group and player entities

---
+++ src/entity/Player.ts
@@ -0,0 +1,47 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from "typeorm";
+import { Guide } from "./Guide";
+import { Group } from "./Group";
+
+@Entity("players")
+export class Player extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @ManyToOne(() => Guide, guide => guide.players, { nullable: false })
+  guide: Guide;
+
+  @ManyToOne(() => Group, group => group.players, { nullable: true })
+  group: Group;
+
+  @Column({ type: "varchar", length: 6, nullable: false })
+  playerCode: string;
+
+  @Column({ type: "varchar", length: 255, nullable: false })
+  email: string;
+
+  @Column({ type: "varchar", length: 255, nullable: true })
+  firstName: string;
+
+  @Column({ type: "varchar", length: 255, nullable: true })
+  lastName: string;
+
+  @Column({ type: "boolean", default: true, nullable: false })
+  active: boolean;
+
+  @Column({ type: "boolean", default: false, nullable: false })
+  acceptedTos: boolean;
+
+  @CreateDateColumn()
+  createdDate: Date;
+
+  @UpdateDateColumn()
+  updatedDate: Date;
+}
