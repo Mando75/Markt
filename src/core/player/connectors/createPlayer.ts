61f52479264a1b313b8d33e5a205301da40759e1
Bryan Muller <bmuller0975@gmail.com>
2019-01-25 23:53:38 -0700

    move resolvers into connectors

---
+++ src/core/player/connectors/createPlayer.ts
@@ -0,0 +1,48 @@
+import { GraphQLContext } from "../../../types/graphql-context";
+import { Guide } from "../../../entity/Guide";
+import { Group } from "../../../entity/Group";
+import { Player } from "../../../entity/Player";
+import { UserInputError, ApolloError } from "apollo-server-express";
+
+export const createPlayer = async (
+  _: any,
+  {
+    playerParams: { guideId, groupId, email, firstName, lastName }
+  }: GQL.ICreatePlayerOnMutationArguments,
+  __: GraphQLContext
+) => {
+  try {
+    const guideP = Guide.findOne({
+      where: { id: guideId },
+      select: ["id"]
+    });
+    const groupP = Group.findOne({
+      where: { id: groupId },
+      select: ["id"]
+    });
+    const player = Player.create({
+      firstName,
+      lastName,
+      email
+    });
+    const [guide, group] = await Promise.all([guideP, groupP]);
+    player.group = group;
+    if (guide) {
+      player.guide = guide;
+      return await player.save();
+    } else {
+      throw new ApolloError(
+        "Invalid guide: Guide does not exist",
+        "BAD_USER_INPUT",
+        {
+          invalidArgs: "guideId"
+        }
+      );
+    }
+  } catch (e) {
+    if (e.extensions.code === "BAD_USER_INPUT") {
+      throw e;
+    }
+    throw new UserInputError("Invalid parameter");
+  }
+};
--- src/core/player/resolvers.ts
+++ src/core/player/resolvers.ts
@@ -1,62 +1,11 @@
 import { ResolverMap } from "../../types/graphql-utils";
-import { GraphQLContext } from "../../types/graphql-context";
-import { Player } from "../../entity/Player";
-import { Guide } from "../../entity/Guide";
-import { Group } from "../../entity/Group";
-import { ApolloError, UserInputError } from "apollo-server-express";
+import { createPlayer, getPlayer } from "./connectors";
 
 export const resolvers: ResolverMap = {
   Query: {
-    async player(
-      _: any,
-      { id }: GQL.IPlayerOnQueryArguments,
-      __: GraphQLContext
-    ) {
-      return await Player.findOne(id);
-    }
+    player: getPlayer
   },
   Mutation: {
-    async createPlayer(
-      _: any,
-      {
-        playerParams: { guideId, groupId, email, firstName, lastName }
-      }: GQL.ICreatePlayerOnMutationArguments,
-      __: GraphQLContext
-    ) {
-      try {
-        const guideP = Guide.findOne({
-          where: { id: guideId },
-          select: ["id"]
-        });
-        const groupP = Group.findOne({
-          where: { id: groupId },
-          select: ["id"]
-        });
-        const player = Player.create({
-          firstName,
-          lastName,
-          email
-        });
-        const [guide, group] = await Promise.all([guideP, groupP]);
-        player.group = group;
-        if (guide) {
-          player.guide = guide;
-          return await player.save();
-        } else {
-          throw new ApolloError(
-            "Invalid guide: Guide does not exist",
-            "BAD_USER_INPUT",
-            {
-              invalidArgs: "guideId"
-            }
-          );
-        }
-      } catch (e) {
-        if (e.extensions.code === "BAD_USER_INPUT") {
-          throw e;
-        }
-        throw new UserInputError("Invalid parameter");
-      }
-    }
+    createPlayer
   }
 };
