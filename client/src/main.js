e1d67986618985862ada023079e3ecf875f64b7c
Adam Gehring <geh12001@byui.edu>
2019-02-07 16:21:11 -0700

    added all files

--- client/src/main.js
+++ client/src/main.js
@@ -3,12 +3,14 @@ import App from './App'
 import router from './router'
 import VueTheMask from 'vue-the-mask'
 import VeeValidate from 'vee-validate'
+import Vuetify from 'vuetify'
 import Meta from 'vue-meta'
 
 Vue.config.productionTip = false
 Vue.use(VueTheMask)
 Vue.use(VeeValidate)
 Vue.use(Meta)
+Vue.use(Vuetify)
 
 /* eslint-disable no-new */
 new Vue({
