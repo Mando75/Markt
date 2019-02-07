9459e5e1d6f0f226274ad8e53a28dbb9898db94c
Adam Gehring <geh12001@byui.edu>
2019-02-07 16:54:16 -0700

    added apollo junk

---
+++ client/src/apollo.js
@@ -0,0 +1,23 @@
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+// HTTP connexion to the API
+const uri = process.env.NODE_ENV === "production" ? "https://markt-dev.herokuapp.com/graphql" : "http://localhost:4000/graphql"
+const httpLink = new HttpLink({
+  // You should use an absolute URL here
+  uri,
+  credentials: 'same-origin'
+
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+export const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+  connectToDevTools : true,
+
+})
--- client/src/main.js
+++ client/src/main.js
@@ -5,16 +5,26 @@ import VueTheMask from 'vue-the-mask'
 import VeeValidate from 'vee-validate'
 import Vuetify from 'vuetify'
 import Meta from 'vue-meta'
+import VueApollo from 'vue-apollo'
+import{apolloClient} from "./apollo";
 
 Vue.config.productionTip = false
 Vue.use(VueTheMask)
 Vue.use(VeeValidate)
 Vue.use(Meta)
 Vue.use(Vuetify)
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  apolloProvider,
   render: h => h(App)
 })
+
+
