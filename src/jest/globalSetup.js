e9ca80cd5d3c47d587cc3142d6f8c40c48065d56
Bryan Muller <bmuller0975@gmail.com>
2019-02-23 00:14:44 -0700

    hopefully fix tests?

--- src/jest/globalSetup.js
+++ src/jest/globalSetup.js
@@ -1,21 +1,10 @@
 process.env.TS_NODE_FILES = "true";
 require("ts-node/register");
-const { spawn } = require("child_process");
+const createConnection = require('../utils/CreateTypeORMConnection').CreateTypeORMConnection
 module.exports = async () => {
-  console.log("In global setup");
   process.env.NODE_ENV = "test";
   process.env.TEST_PORT = 0;
-  const yarn = spawn("yarn", ["run", "migrate:test"]);
-  yarn.stderr.on("data", data => {
-    console.log(`yarn stderr:\n${data}`);
-  });
-  yarn.on("exit", (code, signal) => {
-    if (code) {
-      process.exit(code, signal);
-    }
-  });
-  console.log(`Migration output:`);
-  for await (const data of yarn.stdout) {
-    console.log(data.toString());
-  }
+  const connection = await createConnection();
+  await connection.runMigrations();
+  await connection.close();
 };
