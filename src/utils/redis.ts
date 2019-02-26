8232117b5084d8842ea14c182593ff93628b0ff5
Bryan Müller <starwarsgeek0975@gmail.com>
2019-02-25 19:16:08 -0700

    try another redis connection fix

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,4 +1,8 @@
 import * as Redis from "ioredis";
 
-const url = process.env.REDIS_URL;
+const url =
+  process.env.NODE_ENV === "test" && process.env.CI_ENV !== "true"
+    ? process.env.REDIS_URL + "/1"
+    : process.env.REDIS_URL;
+console.log(url);
 export const redis = new Redis(url as string);
