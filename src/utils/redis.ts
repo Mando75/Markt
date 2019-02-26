0c418ae0f410fa3dbcdb1a27e5c3b6d32d14a120
Bryan Müller <starwarsgeek0975@gmail.com>
2019-02-25 19:07:43 -0700

    try and fix redis url prbs

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,7 +1,7 @@
 import * as Redis from "ioredis";
 
 const url =
-  process.env.NODE_ENV === "test"
+  process.env.NODE_ENV === "test" && process.env.CI_ENV !== "true"
     ? process.env.REDIS_URL + "/1"
     : process.env.REDIS_URL;
 export const redis = new Redis(url as string);
