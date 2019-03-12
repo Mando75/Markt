a76575d4de270ce9a2cdbb30ed49b9080ad56fa8
Adam Gehring <gehringadam@gmail.com>
2019-03-12 16:22:52 -0600

    Added divs

--- client/src/components/GuideFeatures/SessionCreation.vue
+++ client/src/components/GuideFeatures/SessionCreation.vue
@@ -1,24 +1,22 @@
 <template>
   <v-content color="secondary0">
     <Nav />
-    <v-container>
-      <!--<v-item-group>-->
-      <!--<v-item>-->
-      <v-flex align="">
-        <v-btn class="container" id="apple">
-          <v-card>
-            <v-img
-              :src="require('@/assets/marketPainting.jpg')"
-              aspect-ratio="2.75"
-            ></v-img>
-            <v-card-title> TEST APPLE MARKET</v-card-title>
-          </v-card></v-btn
-        ></v-flex
-      >
-      <!--</v-item>-->
-      <!--</v-item-group>-->
-    </v-container></v-content
-  >
+    <v-item-group>
+      <v-layout>
+        <v-item>
+          <v-btn class="container" id="apple">
+            <v-card>
+              <v-img
+                :src="require('@/assets/marketPainting.jpg')"
+                aspect-ratio="2.75"
+              ></v-img>
+              <v-card-title> TEST APPLE MARKET</v-card-title>
+            </v-card></v-btn
+          >
+        </v-item>
+      </v-layout>
+    </v-item-group>
+  </v-content>
 </template>
 
 <script>
