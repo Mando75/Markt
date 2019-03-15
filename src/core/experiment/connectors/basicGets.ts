85b33fb3987fd784e4e472618253a283ce134e31
Bryan Müller <starwarsgeek0975@gmail.com>
2019-03-14 19:06:08 -0600

    optimization?

--- src/core/experiment/connectors/basicGets.ts
+++ src/core/experiment/connectors/basicGets.ts
@@ -1,16 +1,29 @@
+import { GraphQLContext } from "../../../types/graphql-context";
+import { GraphQLResolveInfo } from "graphql";
+import * as graphqlFields from "graphql-fields";
 import { Experiment } from "../../../entity/Experiment";
-import { ExperimentPlayer } from "../../../entity/ExperimentPlayer";
 
 export const getExperiment = async (
   _: any,
-  { id }: GQL.IExperimentOnQueryArguments
+  { id }: GQL.IExperimentOnQueryArguments,
+  __: GraphQLContext,
+  info: GraphQLResolveInfo
 ) => {
-  return await Experiment.findOne(id);
-};
-
-export const getExperimentPlayers = async (exp: Experiment) => {
-  return await ExperimentPlayer.find({
-    relations: ["playerTransactions", "playerTransactions.transaction"],
-    where: { experiment: exp }
-  });
+  /**
+   * Grab only the top level fields being called.
+   * Exclude any relation columns, as they do not exist on the table
+   */
+  const fields = Object.keys(
+    graphqlFields(
+      info,
+      {},
+      { excludedFields: ["__typename", "group", "guide", "closed", "players"] }
+    )
+  ) as (keyof Experiment)[];
+  return fields.length
+    ? await Experiment.findOne(id, {
+        select: fields.filter(f => f !== "scenario"),
+        relations: fields.includes("scenario") ? ["scenario"] : []
+      })
+    : await Experiment.findOne(id);
 };
