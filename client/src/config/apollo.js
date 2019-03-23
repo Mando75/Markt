db4fb3f65f6505773ec671a7a79e325b9c32a640
Bryan Müller <starwarsgeek0975@gmail.com>
2019-03-23 14:11:33 -0600

    add subscription link

--- client/src/config/apollo.js
+++ client/src/config/apollo.js
@@ -1,6 +1,9 @@
 import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { split } from "apollo-link";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
 
 const uri = "/graphql";
 const httpLink = new HttpLink({
@@ -10,10 +13,22 @@ const httpLink = new HttpLink({
   }
 });
 
+const wsUri = "/subscriptions";
+const wsLink = new WebSocketLink({ uri: wsUri, options: { reconnect: true } });
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  httpLink
+);
+
 const cache = new InMemoryCache();
 
 export const apolloClient = new ApolloClient({
-  link: httpLink,
+  link,
   cache,
   connectToDevTools: true
 });
