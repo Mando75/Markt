9e94d382ed9ed68d21048bf2792e05176054659c
Bryan Müller <starwarsgeek0975@gmail.com>
2019-03-28 21:12:11 -0600

    wrap up Joining

--- client/src/components/GuideFeatures/Joining.vue
+++ client/src/components/GuideFeatures/Joining.vue
@@ -1,19 +1,48 @@
 <template>
   <div>
-    <v-layout justify-start column fill-height v-if="apolloLoading">
+    <v-layout
+      align-center
+      justify-start
+      column
+      fill-height
+      v-if="apolloLoading || !experimentPlayerCount"
+    >
       <LoadingBlock />
     </v-layout>
-    <v-layout v-else justify-start column fill-height>
-      <v-flex xs12 d-flex>
-        <h1 class="display-1">
-          You are now live. Waiting for players to join...
-        </h1>
-        <h3 class="display-3">
-          Join Code: <strong>{{ experiment.joinCode }}</strong>
-        </h3>
+    <v-layout v-else justify-start row wrap fill-height>
+      <v-flex xs12 my-3>
+        <span class="display-1">
+          You are now live!
+        </span>
       </v-flex>
-      <v-flex xs12 d-flex>
-        <GuideScenarioInstructions :scenario="experiment.scenario" />
+      <v-flex xs12 my-3>
+        <span class="display-3">
+          Join Code: <strong>{{ experiment.joinCode }}</strong>
+        </span>
+      </v-flex>
+      <v-flex my-3>
+        <span class="display-1">
+          Player Count: {{ experimentPlayerCount.numPlayers }}/{{
+            experiment.scenario.maxPlayerSize
+          }}
+        </span>
+      </v-flex>
+      <v-flex xs12 my-3>
+        <span v-if="experimentFull">Experiment is full</span>
+        <span v-else class="animate-flicker">
+          Waiting for players to join...</span
+        >
+      </v-flex>
+      <v-flex xs4 offset-xs4 align-bottom>
+        <v-card class="ma-3" elevation="4" dark>
+          <v-card-title primary-title>
+            <span>Ready to start the first session?</span>
+            <v-spacer />
+            <v-btn color="primary">
+              Start Session
+            </v-btn>
+          </v-card-title>
+        </v-card>
       </v-flex>
     </v-layout>
   </div>
@@ -40,6 +69,17 @@ export default {
       apolloLoading: 0
     };
   },
+  mounted() {
+    this.$apollo.queries.experimentPlayerCount.skip = false;
+  },
+  computed: {
+    experimentFull() {
+      return this.experimentPlayerCount
+        ? this.experimentPlayerCount.numPlayers ===
+            this.experiment.scenario.maxPlayerSize
+        : false;
+    }
+  },
   apollo: {
     experimentPlayerCount: {
       query: experimentPlayerCount,
@@ -48,6 +88,7 @@ export default {
           experimentId: this.experiment.id
         };
       },
+      skip: true,
       loadingKey: "apolloLoading",
       subscribeToMore: {
         document: experimentPlayerCountChanged,
