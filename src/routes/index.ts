22e7832ca0d60b718c28d04f610b6e7a706e2456
Bryan Müller <starwarsgeek0975@gmail.com>
2019-04-04 22:57:26 -0600

    setup for player uplaod

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from "express";
 import confirmEmail from "./confirmEmail";
 import oauth from "./oauth";
 import * as path from "path";
+import playerInvite from "./players";
 
 const routes = Router();
 
 routes.use(oauth);
 routes.use(confirmEmail);
+routes.use(playerInvite);
 
 routes.get("/", (_, res) => {
   res.sendFile(path.join(__dirname + "../../../client/dist/index.html"));
---
+++ src/routes/players.ts
@@ -0,0 +1,47 @@
+import { Router } from "express";
+import * as multer from "multer";
+import { createReadStream } from "fs";
+import { parse, NODE_STREAM_INPUT } from "papaparse";
+
+const router: Router = Router();
+const upload = multer({ dest: "/tmp/playerInvites" });
+router.post("/players/invite", upload.single("file"), (req, res) => {
+  if (!req.file) {
+    res.status(404).json({ msg: "must attach file" });
+    return;
+  }
+
+  const output: Array<any> = [];
+  const rs = createReadStream(req.file.path);
+  rs.pipe(
+    parse(NODE_STREAM_INPUT, {
+      encoding: "utf-8",
+      header: true,
+      beforeFirstChunk(chunk: string): string | void {
+        const rows = chunk.split(/\r\n|\r|\n/);
+        const headings = rows[0]
+          .split(",")
+          .map(heading => heading.replace(/(-|\s)/gm, "_").toLowerCase());
+
+        for (const e of ["first_name", "last_name", "email"]) {
+          if (!headings.includes(e)) {
+            rs.close();
+            res.status(422).json({ msg: `missing '${e}' column` });
+            return;
+          }
+        }
+
+        rows[0] = headings.join();
+        return rows.join("\n");
+      }
+    })
+  )
+    .on("data", data => {
+      output.push(data);
+    })
+    .on("end", () => {
+      res.json(output);
+    });
+});
+
+export default router;
