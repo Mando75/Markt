8e2ffe31b2b18b6f27c7d9c515890ae1fd9bede1
Bryan Muller <bmuller0975@gmail.com>
2019-05-11 23:19:16 -0600

    add dataloader and typeorm-loader

---
+++ src/utils/graphqlLoaders/loader.ts
@@ -0,0 +1,201 @@
+import { Connection } from "typeorm";
+import { GraphQLResolveInfo } from "graphql";
+import * as crypto from "crypto";
+
+import { select, graphqlFields, Selection } from "./select";
+
+export type LoaderOptions = {
+  /**
+   * Time-to-live for cache.
+   */
+  ttl?: number;
+};
+
+type QueueItem = {
+  many: boolean;
+  key: string;
+  batchIdx: number;
+  fields: Selection | null;
+  where: any;
+  resolve: (value?: any) => any;
+  reject: (reason: any) => void;
+  entity: Function | string;
+};
+
+/**
+ * GraphQLDatabaseLoader is a caching loader that folds a batch of different database queries into a singular query.
+ */
+export class GraphQLDatabaseLoader {
+  private _queue: QueueItem[] = [];
+  private _cache: Map<string, Promise<any>> = new Map();
+  private _immediate?: NodeJS.Immediate;
+
+  /**
+   * Constructs an instance.
+   * @param {Connection} connection The database connection.
+   * @param {LoaderOptions} options (optional) Loader options.
+   */
+  constructor(
+    public connection: Connection,
+    public options: LoaderOptions = {}
+  ) {}
+
+  /**
+   * Load an entity from the database.
+   * @param {typeof BaseEntity|string} entity The entity type to load.
+   * @param where Query conditions.
+   * @param {GraphQLResolveInfo} info (optional) GraphQL resolver information. If not provided, all fields are returned.
+   * @returns {Promise<T>}
+   */
+  async loadOne<T>(
+    entity: Function | string,
+    where: Partial<T>,
+    info?: GraphQLResolveInfo
+  ): Promise<T | undefined> {
+    // Create a md5 hash.
+    const hash = crypto.createHash("md5");
+    // Get the fields queried by GraphQL.
+    const fields = info ? graphqlFields(info) : null;
+    // Generate a key hash from the query parameters.
+    const key = hash
+      .update(JSON.stringify([where, fields]))
+      .digest()
+      .toString("hex");
+    // If the key matches a cache entry, return it.
+    if (this._cache.has(key)) return this._cache.get(key);
+    // If we have an immediate scheduled, cancel it.
+    if (this._immediate) {
+      clearImmediate(this._immediate);
+    }
+    // Create a promise.
+    const promise = new Promise<T | undefined>((resolve, reject) => {
+      // Push resolve/reject to the queue.
+      this._queue.push({
+        many: false,
+        batchIdx: this._queue.length,
+        key,
+        where,
+        fields,
+        resolve,
+        reject,
+        entity
+      });
+    });
+    // Set a new immediate.
+    this._immediate = setImmediate(() => this.processAll());
+    // Cache the promise.
+    this._cache.set(key, promise);
+    // Return the promise.
+    return promise;
+  }
+
+  /**
+   * Load multiple entities that meet the same criteria .
+   * @param {Function|string} entity The entity type to load.
+   * @param {Partial<T>} where The conditions to match.
+   * @param {GraphQLResolveInfo} info (optional)  GraphQL resolver information. If not provided, all fields are returned.
+   * @returns {Promise<T?[]>}
+   */
+  async loadMany<T>(
+    entity: Function | string,
+    where: Partial<T>,
+    info?: GraphQLResolveInfo
+  ): Promise<(T | undefined)[]> {
+    // Create a md5 hash.
+    const hash = crypto.createHash("md5");
+    // Get the fields queried by GraphQL.
+    const fields = info ? graphqlFields(info) : null;
+    // Generate a key hash from the query parameters.
+    const key = hash
+      .update(JSON.stringify([where, fields]))
+      .digest()
+      .toString("hex");
+    // If the key matches a cache entry, return it.
+    if (this._cache.has(key)) return this._cache.get(key);
+    // If we have an immediate scheduled, cancel it.
+    if (this._immediate) {
+      clearImmediate(this._immediate);
+    }
+    // Create a promise.
+    const promise = new Promise<(T | undefined)[]>((resolve, reject) => {
+      // Push resolve/reject to the queue.
+      this._queue.push({
+        many: true,
+        batchIdx: this._queue.length,
+        key,
+        where,
+        fields,
+        resolve,
+        reject,
+        entity
+      });
+    });
+    // Set a new immediate.
+    this._immediate = setImmediate(() => this.processAll());
+    // Cache the promise.
+    this._cache.set(key, promise);
+    // Return the promise.
+    return promise;
+  }
+
+  /**
+   * Load multiple entities with different criteria.
+   * @param {Function|string} entity The entity type to load.
+   * @param {Partial<T>[]} where A series of conditions to match.
+   * @param {GraphQLResolveInfo} info (optional)  GraphQL resolver information. If not provided, all fields are returned.
+   * @returns {Promise<T?[]>}
+   */
+  async batchLoadMany<T>(
+    entity: Function | string,
+    where: Partial<T>[],
+    info?: GraphQLResolveInfo
+  ): Promise<(T | undefined)[]> {
+    return await Promise.all(where.map(w => this.loadOne(entity, w, info)));
+  }
+
+  /**
+   * Clears the loader cache.
+   */
+  clear() {
+    this._cache.clear();
+  }
+
+  /**
+   * Process and clear the current queue.
+   * @returns {Promise<void>}
+   */
+  protected async processAll() {
+    // Clear and capture the current queue.
+    const queue = this._queue.splice(0, this._queue.length);
+    try {
+      // Create a new QueryBuilder instance.
+      return await this.connection.transaction(async entityManager => {
+        // const now = Date.now().toString(16);
+        return queue.map(q => {
+          const name = typeof q.entity == "string" ? q.entity : q.entity.name;
+          const alias = "Q";
+          let qb = entityManager.getRepository(name).createQueryBuilder(alias); //.getRepository(q.entity).createQueryBuilder();
+          // qb = qb.from(name, alias);
+          qb = select(
+            name,
+            q.fields,
+            entityManager.connection,
+            qb as any,
+            alias
+          );
+          qb = qb.where(q.where);
+          const promise = q.many ? qb.getMany() : qb.getOne();
+          return promise.then(q.resolve, q.reject).finally(() => {
+            this._cache.delete(q.key);
+          });
+        });
+      });
+    } catch (e) {
+      // An error occurred, reject the entire queue.
+      queue.forEach(q => {
+        q.reject(e);
+        this._cache.delete(q.key);
+      });
+    }
+  }
+}
---
+++ src/utils/graphqlLoaders/select.ts
@@ -0,0 +1,186 @@
+import {
+  ASTNode,
+  FieldNode,
+  GraphQLResolveInfo,
+  Kind,
+  OperationDefinitionNode,
+  SelectionNode,
+  ValueNode
+} from "graphql";
+import { BaseEntity, Connection, SelectQueryBuilder } from "typeorm";
+import { RelationMetadata } from "typeorm/metadata/RelationMetadata";
+
+export type Hash<T> = {
+  [key: string]: T;
+};
+
+export type Selection = {
+  arguments?: Hash<{ name: string; value: any }>;
+  children?: Hash<Selection>;
+};
+
+function parseLiteral(ast: ValueNode): any {
+  switch (ast.kind) {
+    case Kind.STRING:
+    case Kind.BOOLEAN:
+      return ast.value;
+    case Kind.INT:
+    case Kind.FLOAT:
+      return parseFloat(ast.value);
+    case Kind.OBJECT: {
+      const value = Object.create(null);
+      ast.fields.forEach(field => {
+        value[field.name.value] = parseLiteral(field.value);
+      });
+      return value;
+    }
+    case Kind.LIST:
+      return ast.values.map(parseLiteral);
+    default:
+      return null;
+  }
+}
+
+function getSelections(
+  ast: OperationDefinitionNode
+): ReadonlyArray<SelectionNode> {
+  if (
+    ast &&
+    ast.selectionSet &&
+    ast.selectionSet.selections &&
+    ast.selectionSet.selections.length
+  ) {
+    return ast.selectionSet.selections;
+  }
+  return [];
+}
+
+function isFragment(ast: ASTNode) {
+  return ast.kind === "InlineFragment" || ast.kind === "FragmentSpread";
+}
+
+function getAST(ast: ASTNode, info: GraphQLResolveInfo) {
+  if (ast.kind === "FragmentSpread") {
+    const fragmentName = ast.name.value;
+    return info.fragments[fragmentName];
+  }
+  return ast;
+}
+
+function flattenAST(
+  ast: ASTNode,
+  info: GraphQLResolveInfo,
+  obj: Hash<Selection> = {}
+): Hash<Selection> {
+  return getSelections(ast as OperationDefinitionNode).reduce(
+    (flattened, n) => {
+      if (isFragment(n)) {
+        flattened = flattenAST(getAST(n, info), info, flattened);
+      } else {
+        const node: FieldNode = n as FieldNode;
+        const name = (node as FieldNode).name.value;
+        if (flattened[name]) {
+          Object.assign(
+            flattened[name].children,
+            flattenAST(node, info, flattened[name].children)
+          );
+        } else {
+          flattened[name] = {
+            arguments: node.arguments
+              ? node.arguments
+                  .map(({ name, value }) => ({
+                    [name.value]: parseLiteral(value)
+                  }))
+                  .reduce((p, n) => ({ ...p, ...n }), {})
+              : {},
+            children: flattenAST(node, info)
+          };
+        }
+      }
+      return flattened;
+    },
+    obj
+  );
+}
+
+export function graphqlFields(
+  info: GraphQLResolveInfo,
+  obj: Hash<Selection> = {}
+): Selection {
+  const fields = info.fieldNodes;
+  return { children: fields.reduce((o, ast) => flattenAST(ast, info, o), obj) };
+}
+
+export function select(
+  model: Function | string,
+  selection: Selection | null,
+  connection: Connection,
+  qb: SelectQueryBuilder<typeof BaseEntity>,
+  alias: string,
+  history?: Set<RelationMetadata>
+) {
+  const meta = connection.getMetadata(model);
+  if (selection && selection.children) {
+    const fields = meta.columns.filter(field => {
+      return field.propertyName in selection.children!;
+    });
+    fields.forEach(
+      field =>
+        (qb = qb.addSelect(
+          `${alias}.${field.propertyName}`,
+          `${alias}_${field.propertyName}`
+        ))
+    );
+    const relations = meta.relations;
+    relations.forEach(relation => {
+      if (relation.propertyName in selection.children!) {
+        const target = connection.getRepository(relation.target).target;
+        const name = typeof target == "string" ? target : target.name;
+        const childAlias = alias + "_" + name;
+        if (relation.inverseRelation) {
+          // qb = qb.addFrom(
+          //   relation.inverseRelation.entityMetadata.targetName,
+          //   relation.inverseRelation.entityMetadata.targetName
+          // );
+          qb = qb.leftJoin(alias + "." + relation.propertyName, childAlias);
+          qb = select(
+            relation.inverseEntityMetadata.target,
+            selection.children![relation.propertyName],
+            connection,
+            qb,
+            childAlias
+          );
+        }
+      }
+    });
+  } else if (selection === null) {
+    history = history || new Set();
+    const relations = meta.relations;
+    relations.forEach(relation => {
+      const childAlias = `${alias}_${relation.propertyName}`;
+      if (relation.inverseRelation) {
+        if (history!.has(relation.inverseRelation)) {
+          qb = qb.addSelect(alias);
+          return;
+        }
+        history!.add(relation);
+        qb = qb.addFrom(
+          relation.inverseRelation.entityMetadata.targetName,
+          relation.inverseEntityMetadata.targetName
+        );
+        qb = qb.leftJoin(alias + "." + relation.propertyName, childAlias);
+        qb = select(
+          relation.inverseEntityMetadata.targetName,
+          null,
+          connection,
+          qb,
+          childAlias,
+          history
+        );
+      } else {
+        qb = qb.addSelect(`${alias}.${relation.propertyName}`, childAlias);
+      }
+    });
+  }
+  return qb;
+}
